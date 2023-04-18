0ac9c57a760942425901dd248437d1bf4c5aadb3
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-04-19 01:19:25 +0530

    Added Icons in Tabs

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -1,5 +1,33 @@
 import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default () => {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="feed"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="search" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Messages"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="envelope" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
