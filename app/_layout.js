74a96419017627bef9dfc9c8f28047989ebe9d1c
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-04-19 00:53:21 +0530

    Tried Tabs

--- app/_layout.js
+++ app/_layout.js
@@ -1,18 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 
-export default () => {
-  return (
-    <Stack
-      screenOptions={{
-        headerTintColor: "red",
-        headerStyle: { backgroundColor: "blue" },
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-    </Stack>
-  );
-};
+export default Tabs;
+
+// export default () => {
+//   return (
+//     <Stack
+//       screenOptions={{
+//         headerTintColor: "red",
+//         headerStyle: { backgroundColor: "blue" },
+//         headerTitleStyle: {
+//           fontWeight: "bold",
+//         },
+//       }}
+//     >
+//       <Stack.Screen name="index" options={{ title: "Home" }} />
+//       <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+//     </Stack>
+//   );
+// };
